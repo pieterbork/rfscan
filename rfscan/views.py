56396860c70ae20428f0027459770df6f2b15ccd
pieterbork <pieter.bork@colorado.edu>
2018-04-08 20:07:39 +0000

    pdfkit, results to pdf

--- rfscan/views.py
+++ rfscan/views.py
@@ -1,10 +1,13 @@
-from flask import render_template,request
+from flask import render_template,request, make_response
 from flask_socketio import emit
 from time import time,strftime
 from random import randrange
 from rfscan import app,socketio
 import thread
 import datetime
+
+import pdfkit
+import os
 
 from rfscan.core import database
 from rfscan.core.utils import *
@@ -15,6 +18,47 @@ def dash():
 	jobs = database.get_records_from_table("Jobs")
 	
 	return render_template("dashboard.html", jobs=jobs)
+
+@app.route('/to_pdf/<job>')
+def to_pdf(job):
+	job_id = database.get_job_id(job)
+	wifi_records = database.get_records_from_table("Wifi", job_id)	
+	zigbee_records = database.get_records_from_table("Zigbee", job_id)	
+	bt_records = database.get_records_from_table("Bluetooth", job_id)	
+	charts = {}
+
+	#Build the tables/charts
+	top_talkers = build_top_talkers_table(wifi_records)
+	ssids = build_unique_ssids_table(wifi_records)
+	charts['ssids_per_channel'] = build_chart_js("ssids_per_channel", wifi_records)
+	charts['packets_per_channel'] = build_chart_js("packets_per_channel", wifi_records)
+
+	#Make sure colors are consistent
+	colors = generate_colors(charts['packets_per_channel']['channel_names'])
+	charts['packets_per_channel']['colors'] = colors.values()
+	charts['ssids_per_channel']['colors'] = [colors[ch] for ch in colors.keys() if ch in charts['ssids_per_channel']['channel_names']]
+	
+	rendered = render_template("results.html", 
+				records={
+					"Wifi":sorted(wifi_records, key=lambda x: x[6], reverse=True),
+					"Wifi_fields": ["SSID", "MAC 1", "MAC 2", "MAC 3", "Frequency", "Count"],
+					"Wifi_ssids": ssids,
+					"top_talkers": top_talkers,
+					"Zigbee":sorted(zigbee_records, key=lambda x: x[7], reverse=True), 
+					"Zigbee_fields": ["Source", "Destination", "Ext Source", "Ext Dest", "Sec Source", "Sec Dest", "Count"],
+					"Bluetooth":sorted(bt_records, key=lambda x: x[3], reverse=True),
+					"Bluetooth_fields": ["Channel", "MAC", "Count"],
+					"protocols": ["Wifi", "Zigbee", "Bluetooth"]
+				}, 
+				charts=charts,
+				job=job)
+	new_path = os.getcwd() + '/rfscan/static/'
+	rendered = rendered.replace('/static/', new_path)
+	pdf = pdfkit.from_string(rendered, False)
+	resp = make_response(pdf)
+	resp.headers['Content-Type'] = 'application/pdf'
+	resp.headers['Content-Disposition'] = 'inline; filename=output.pdf'
+	return resp
 
 @app.route('/scan', methods=["GET", "POST"])
 def scan():
